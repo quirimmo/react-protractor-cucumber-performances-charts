9402418e26d6322979d0c1b8e8cbe0f093bdb814
quirimmo <quirimmo@gmail.com>
2018-03-26 16:23:35 +0100

    fixed the css issues and cleaned the project from useless stuff

--- src/components/presentationals/App.component.spec.tsx
+++ src/components/presentationals/App.component.spec.tsx
@@ -6,8 +6,7 @@ import { shallow } from 'enzyme';
 
 it('fake', () => {
 	const component = shallow(<App />);
-
-	expect(2).toBe(2);
+	expect(3).toBe(3);
 	expect(component).toBeDefined();
 	// expect(result).toBeTruthy();
 });
