9b883c35aae5b99f09f106c8863921e1ccac1b0f
quirimmo <quirimmo@gmail.com>
2018-04-08 00:46:31 +0100

    going on with unit tests

--- src/components/presentationals/AppMainContent.component.spec.tsx
+++ src/components/presentationals/AppMainContent.component.spec.tsx
@@ -3,6 +3,9 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import AppMainContent from './AppMainContent.component';
 import { Row, Col } from 'reactstrap';
 import { Route, Switch, Redirect } from 'react-router';
+import ScenariosPage from '../containers/ScenariosPage.component';
+import StepsPage from '../containers/StepsPage.component';
+import ScenarioDetailsPage from '../containers/ScenarioDetailsPage.component';
 
 let component: any;
 
@@ -53,21 +56,82 @@ describe('App Presentational Component', () => {
 
 	describe('Routes', () => {
 		describe('Main Route', () => {
+			let mainRoute: any;
+			beforeEach(() => {
+				mainRoute = component.find(Route).at(0);
+			});
+
 			it('should define the path main', () => {
-				// const mainRoute = component.find(Route);
-				// console.log(mainRoute.props().path)
-				// expect(mainRoute.props())
-				// <Route path="/main" render={this.getScenariosPageRoute} />
+				expect(mainRoute.props().path).toEqual('/main');
+			});
+
+			it('should define the render bound to the function getScenariosPageRoute', () => {
+				expect(mainRoute.props().render).toEqual(component.instance().getScenariosPageRoute);
+			});
+		});
+
+		describe('Steps Route', () => {
+			let stepsRoute: any;
+			beforeEach(() => {
+				stepsRoute = component.find(Route).at(1);
+			});
+
+			it('should define the path steps', () => {
+				expect(stepsRoute.props().path).toEqual('/steps');
+			});
+
+			it('should define the render bound to the function getStepsPageRoute', () => {
+				expect(stepsRoute.props().render).toEqual(component.instance().getStepsPageRoute);
+			});
+		});
+
+		describe('Scemario Details Route', () => {
+			let scenarioDetailsRoute: any;
+			beforeEach(() => {
+				scenarioDetailsRoute = component.find(Route).at(2);
+			});
+
+			it('should define the path scenario-details', () => {
+				expect(scenarioDetailsRoute.props().path).toEqual('/scenario-details');
+			});
+
+			it('should define the render bound to the function getScenarioDetailsPageRoute', () => {
+				expect(scenarioDetailsRoute.props().render).toEqual(component.instance().getScenarioDetailsPageRoute);
 			});
 		});
 	});
 
 	describe('Redirect', () => {
+		let redirect: any;
+
+		beforeEach(() => {
+			redirect = component.find(Redirect);
+		});
+
+		it('should redirect from all the paths', () => {
+			expect(redirect.props().from).toEqual('/');
+		});
+
+		it('should redirect to main', () => {
+			expect(redirect.props().to).toEqual('main');
+		});
 	});
+
 	describe('getScenariosPageRoute', () => {
+		it('should return a ScenariosPage component', () => {
+			expect(component.instance().getScenariosPageRoute()).toEqual(<ScenariosPage />);
+		});
 	});
+
 	describe('getStepsPageRoute', () => {
+		it('should return a StepsPage component', () => {
+			expect(component.instance().getStepsPageRoute()).toEqual(<StepsPage />);
+		});
 	});
+
 	describe('getScenarioDetailsPageRoute', () => {
+		it('should return a ScenarioDetailsPage component', () => {
+			expect(component.instance().getScenarioDetailsPageRoute()).toEqual(<ScenarioDetailsPage />);
+		});
 	});
 });
